eb07cf3a890829998aa194b0b910236d50e7ade6
nana <nana.nimako@uvm.edu>
2020-09-02 14:20:13 -0400

    Just got deliverable 1, step 52 working

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,9 +1,30 @@
 var controllerOptions = {};
+var x = window.innerWidth; //get the width of current window
+var y = window.innerHeight;  //get the height of current window
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+/*var xRandNum = getRndInteger(-1,1) + (Math.random());
+console.log(xRandNum);
+
+var yRandNum = getRndInteger(-1,1) + (Math.random());
+console.log(yRandNum);*/
+
 Leap.loop(controllerOptions, function(frame)
 {
+	clear();
 
+	var xRandNum = getRndInteger(-1,1) + (Math.random());
+	console.log(xRandNum);
+
+	var yRandNum = getRndInteger(-1,1) + (Math.random());
+	console.log(yRandNum);
+
+
+
+	var i = 0;
+	console.log(i);
+	circle((x/2) + xRandNum, (y/2) + yRandNum, 50);
 }
 );
-
-var i = 0;
-console.log(i);
