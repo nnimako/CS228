beb25f6fc459050d562b0fc1e3b878a903752cd5
nana <nana.nimako@uvm.edu>
2020-09-21 04:18:32 -0400

    Del03 step 52 complete

--- Playback.js
+++ Playback.js
@@ -1,118 +1,80 @@
-var controllerOptions = {};
-// var x = window.innerWidth; //get the width of current window
-// var y = window.innerHeight;  //get the height of current window
+var oneFrameOfData = nj.array([[[  269.9921, 132.20259,   158.229,  269.9921, 132.20259,   158.229],
+        [  269.9921, 132.20259,   158.229, 192.33977, 106.40473,   110.729],
+        [ 192.33977, 106.40473,   110.729, 165.39616,  84.58538,   77.8858],
+        [ 165.39616,  84.58538,   77.8858, 107.55907,  76.24989,   56.3387]],
+       [[ 342.27929, 207.79095,    156.12, 330.97764, 260.22901,   85.1181],
+        [ 330.97764, 260.22901,   85.1181, 331.01061, 187.27636,    47.979],
+        [ 331.01061, 187.27636,    47.979, 329.29353, 120.26744,   33.1969],
+        [ 329.29353, 120.26744,   33.1969,  327.4065,  65.16927,    26.298]],
+       [[ 430.33152, 213.47351,   155.592, 488.61252, 261.89719,   88.6403],
+        [ 488.61252, 261.89719,   88.6403, 509.21379, 172.74747,   48.2519],
+        [ 509.21379, 172.74747,   48.2519, 491.20352,  88.44299,     33.18],
+        [ 491.20352,  88.44299,     33.18, 468.19073,   25.7566,   27.9061]],
+       [[ 518.31415, 208.07795,   155.886, 637.59096, 248.61278,   97.4179],
+        [ 637.59096, 248.61278,   97.4179, 648.83767, 171.89042,   58.9564],
+        [ 648.83767, 171.89042,   58.9564, 619.01077,  91.45861,   43.9179],
+        [ 619.01077,  91.45861,   43.9179, 585.84203,  30.13116,   38.7855]],
+       [[ 599.38232, 177.44454,   157.053, 765.95128, 219.61167,   105.958],
+        [ 765.95128, 219.61167,   105.958, 770.41465, 169.11946,   74.0835],
+        [ 770.41465, 169.11946,   74.0835, 738.65458, 119.03801,   61.5179],
+        [ 738.65458, 119.03801,   61.5179, 695.05371,  67.79745,   54.5071]]]);
 
-var rawXMin, rawXMax, rawYMin, rawYMax;
+var anotherFrameData = nj.array([[[ 1178.71947,   156.5446,    255.038, 1178.71947,   156.5446,    255.038],
+        [ 1178.71947,   156.5446,    255.038,  593.82117,  294.46779,    223.518],
+        [  593.82117,  294.46779,    223.518,  294.75326,  361.86131,    196.614],
+        [  294.75326,  361.86131,    196.614,   61.99185,  415.39271,     179.39]],
+       [[ 1411.42164,  310.83337,    253.019,   776.9008,  634.63283,    206.238],
+        [   776.9008,  634.63283,    206.238,  351.41735,  666.30894,    172.621],
+        [  351.41735,  666.30894,    172.621,  166.08134,  515.76251,    162.806],
+        [  166.08134,  515.76251,    162.806,  140.23112,  383.26112,    164.713]],
+       [[ 1587.11144,  316.27504,    246.043, 1122.27347,  613.44089,    196.389],
+        [ 1122.27347,  613.44089,    196.389,   659.0127,  656.17259,    158.289],
+        [   659.0127,  656.17259,    158.289,  372.97177,   488.5252,    149.086],
+        [  372.97177,   488.5252,    149.086,  293.45716,  349.81031,    153.327]],
+       [[ 1754.29268,  296.62425,    238.899,  1479.6755,  546.52768,    189.988],
+        [  1479.6755,  546.52768,    189.988, 1041.04205,  649.04236,    156.905],
+        [ 1041.04205,  649.04236,    156.905,  689.90015,   586.9903,    140.027],
+        [  689.90015,   586.9903,    140.027,  469.96009,  492.77352,    133.145]],
+       [[ 1879.14045,   223.4222,    231.184, 1771.17588,  450.08323,    183.961],
+        [ 1771.17588,  450.08323,    183.961, 1483.76188,  566.15445,    157.666],
+        [ 1483.76188,  566.15445,    157.666, 1226.67463,   550.9996,    145.271],
+        [ 1226.67463,   550.9996,    145.271,  987.78894,  488.53348,    138.277]]]);
 
-rawXMin = 5000;
-rawYMin = 5000;
-rawXMax = -5000;
-rawYMax = -5000;
+var frameIndex = 0;
+var flipped = 0;
 
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-
-function HandleHand(hand){
-	var fingers = hand.fingers;
-	for (var f=0; f<fingers.length; f++){
-		var finger = fingers[f];
-		for(var b=finger.bones.length-1; b>=0; b--){
-			HandleBone(finger.bones[b], 1.5);
+function draw(){
+	clear();
+	for(var i=0; i<5; i++){
+		for(var j=0; j<4; j++){
+			if (flipped == 0) {
+				var xStart = anotherFrameData.get(i,j,0);
+				var yStart = anotherFrameData.get(i,j,1);
+				var zStart = anotherFrameData.get(i,j,2);
+				var xEnd = anotherFrameData.get(i,j,3);
+				var yEnd = anotherFrameData.get(i,j,4);
+				var zEnd = anotherFrameData.get(i,j,5);
+				line(xStart, yStart,xEnd,yEnd);
+			} else if (flipped == 1) {
+				var xStart = oneFrameOfData.get(i,j,0);
+				var yStart = oneFrameOfData.get(i,j,1);
+				var zStart = oneFrameOfData.get(i,j,2);
+				var xEnd = oneFrameOfData.get(i,j,3);
+				var yEnd = oneFrameOfData.get(i,j,4);
+				var zEnd = oneFrameOfData.get(i,j,5);
+				line(xStart, yStart,xEnd,yEnd);
+			}
 		}
 	}
+	
+	if (frameIndex == 100){
+		frameIndex = 0;
+		if (flipped == 1) {
+			flipped = 0;
+		} else{
+			flipped = 1;
+		}
+	}
+	frameIndex+=1;
+	// console.log(oneFrameOfData.toString());
 }
-
-function HandleFrame(frame){
-	if (frame.hands.length == 1) {
-		var hand = frame.hands[0];
-		HandleHand(hand);
-	}
-}
-
-/*function HandleFinger(finger){
-	for(var b=0; b<finger.bones.length; b++){
-		var bone = finger.bones[b];
-		var strkWeight = 1.5;
-		
-		HandleBone(bone, strkWeight);
-	}
-}*/
-
-function HandleBone(bone,strkWeight){
-	var bone_base = bone.prevJoint;
-	var bone_tip = bone.nextJoint;
-
-	var xb = bone_base[0];
-	var yb = bone_base[1];
-	var zb = bone_base[2];
-
-	var xt = bone_tip[0];
-	var yt = bone_tip[1];
-	var zt = bone_tip[2];
-
-	[xb,yb] = TransformCoordinates(xb,yb);
-	[xt,yt] = TransformCoordinates(xt,yt);
-
-	if (bone.type == 3){
-		strkWeight += 1.5;
-		stroke(50);
-	} 
-	if (bone.type == 2){
-		strkWeight += 4;
-		stroke(80);
-	} 
-	if (bone.type == 1){
-		strkWeight += 8;
-		stroke(210);
-	}
-	if (bone.type == 0){
-		strkWeight += 11;
-		stroke(215);
-	}
-
-	strokeWeight(strkWeight);
-
-	line(xt,window.innerHeight - yt,xb, window.innerHeight - yb);
-}
-
-function TransformCoordinates(x,y){
-	if (x < rawXMin){
-		rawXMin = x;
-	}
-	if (x > rawXMax){
-		rawXMax = x;
-	}
-	if (y < rawYMin){
-		rawYMin = y;
-	}
-	if (y > rawYMax){
-		rawYMax = y;
-	}
-
-	//code from: https://stackoverflow.com/questions/929103/convert-a-number-range-to-another-range-maintaining-ratio
-	x = ( (x - rawXMin) / (rawXMax - rawXMin) ) * (window.innerWidth - 0) + 0;
-	// console.log(x);
-
-	var y = ( (y - rawYMin) / (rawYMax - rawYMin) ) * (window.innerHeight - 0) + 0;
-
-	return [x,y];
-}
-
-Leap.loop(controllerOptions, function(frame)
-{
-	clear();
-
-	// var xRandNum = getRndInteger(-1,1) + (Math.random());
-	// // console.log(xRandNum);
-
-	// var yRandNum = getRndInteger(-1,1) + (Math.random());
-	// // console.log(yRandNum);
-
-
-
-	// var i = 0;
-	// console.log(i);
-	// circle((x/2) + xRandNum, (y/2) + yRandNum, 50);
-	HandleFrame(frame);
-});
